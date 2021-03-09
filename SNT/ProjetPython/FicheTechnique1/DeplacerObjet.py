8c9610b9fc2045b63f8f5930fcede7fdc9dc629c
pierre.ghesquiere@lilo.org <pierre.ghesquiere@lilo.org>
2021-03-09 18:22:30 +0100

    erreur

---
+++ SNT/ProjetPython/FicheTechnique1/DeplacerObjet.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+import keyboard
+
+img = np.zeros((500,500))
+monObjetLigne   = 100
+monObjetColonne = 300
+monObjetHauteur = 50
+monObjetLargeur = 75
+img[monObjetLigne:monObjetLigne+monObjetHauteur,monObjetColonne:monObjetColonne+monObjetLargeur] = 1
+
+while not keyboard.is_pressed('ctrl'):
+    
+    cv2.imshow('image1',img)
+    cv2.waitKey(1)
+    
+    img[monObjetLigne:monObjetLigne+monObjetHauteur,monObjetColonne:monObjetColonne+monObjetLargeur] = 0
+    monObjetColonne = monObjetColonne-10
+    img[monObjetLigne:monObjetLigne+monObjetHauteur,monObjetColonne:monObjetColonne+monObjetLargeur] = 1
+       
+    
+cv2.destroyWindow("image1")
+
+
+
